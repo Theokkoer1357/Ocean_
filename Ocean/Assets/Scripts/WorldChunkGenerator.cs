7b25894d77b9cf8bb14bf148857172ec9ac6d93b
Theokkoer1357 <49214970+Theokkoer1357@users.noreply.github.com>
2020-04-27 14:45:00 -0400

    WorldChunkGenerator Works

---
+++ Ocean/Assets/Scripts/WorldChunkGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldChunkGenerator : MonoBehaviour
+{
+    public int XChunkLength;
+    public int YChunkLength;
+    public GameObject Chunk;
+    public void Awake()
+    {
+        for (int i = 0; i < XChunkLength; i++)
+        {
+            for (int j = 0; j < YChunkLength; j++)
+            {
+                Instantiate(Chunk, new Vector3(i * Chunk.transform.localScale.x, j * Chunk.transform.localScale.x, 0), Quaternion.identity);
+            }
+        }
+    }
+}

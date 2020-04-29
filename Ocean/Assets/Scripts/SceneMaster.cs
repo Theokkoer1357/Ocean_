e2e7a17c8ef5f260d31d603370cd95a8ce273149
Theokkoer1357 <49214970+Theokkoer1357@users.noreply.github.com>
2020-04-28 20:20:22 -0400

    Scene One Ui Done

---
+++ Ocean/Assets/Scripts/SceneMaster.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMaster : MonoBehaviour
+{
+    public void TransitionScene(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+    
+}

89b36fc0d16525d326203e92d64d8aaf3153ea86
Theokkoer1357 <49214970+Theokkoer1357@users.noreply.github.com>
2020-05-06 18:22:45 -0400

    help

--- Ocean/Assets/Scripts/GetNextTo.cs
+++ Ocean/Assets/Scripts/GetNextTo.cs
@@ -10,10 +10,16 @@ public class GetNextTo : MonoBehaviour
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     
     public void RayOut()
     {
         if (!b_MaskObjectUse)
+=======
+    public void Start()
+    {
+        if (b_MaskObjectUse)
+>>>>>>> parent of 19df8f0... Chasms
 =======
     public void Start()
     {
--- Ocean/Assets/Scripts/Map.cs
+++ Ocean/Assets/Scripts/Map.cs
@@ -30,6 +30,7 @@ public class Map
     }
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     Texture2D Chasms(Texture2D T)
     {
         int T_ = Random.Range(0, T.width);
@@ -40,6 +41,8 @@ public class Map
         }
         return T;
     }
+=======
+>>>>>>> parent of 19df8f0... Chasms
 =======
 >>>>>>> parent of 19df8f0... Chasms
 =======
